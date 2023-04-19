2f0bafd654cc4b44ec3dd8ff7792fcbe256c922c
mattleesounds <mattleesounds@gmail.com>
2023-04-19 13:03:17 -0500

    rebuilt auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "polp324ac576d": {
+    "YOUR_AUTH_RESOURCE_NAME": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
